3773f712c181ee10451c81882b909c1bf6079fcd
HiroIshida <spitfire.docomo@gmail.com>
2023-07-02 03:54:51 +0900

    Change pr2 initial configuration

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ from skrobot.interfaces.ros.pr2 import PR2ROSRobotInterface
 
 model = PR2()
 model.reset_manip_pose()
+print(model.l_shoulder_pan_joint.joint_angle())
+model.l_shoulder_pan_joint.joint_angle(model.l_shoulder_pan_joint.joint_angle() + 0.4)
+model.r_shoulder_pan_joint.joint_angle(model.r_shoulder_pan_joint.joint_angle() - 0.4)
 ri = PR2ROSRobotInterface(model)
 ri.angle_vector(model.angle_vector())
